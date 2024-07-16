37ab273659cf3e4af473f5247587017b8b747b94
Mateus Gomes <sec.mateuslgomes@gmail.com>
2024-07-16 12:31:53 -0300

    Create Project

---
+++ src/main/java/com/mlgsec/api_admanager/ApiAdmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mlgsec.api_admanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlgsec/api_admanager/ApiAdmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlgsec.api_admanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
